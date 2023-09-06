cd20985a94cf116b84b35a290e61c8db4aa9d22b
furkanqq <133240374+furkanqq@users.noreply.github.com>
2023-09-06 11:23:16 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
+class DesktopNotification extends Component {
+  constructor() {
+    super();
+    this.showNotification = this.showNotification.bind(this);
+  }
+
+  componentDidMount() {
+    if (!("Notification" in window)) {
+      console.log("Browser does not support desktop notification");
+    } else {
+      Notification.requestPermission();
+    }
+  }
+
+  showNotification() {
+    new Notification('Hello World')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.showNotification}>Show notification</button></div>
+    );
+  }
 }
 
-export default App;
+export default DesktopNotification;
